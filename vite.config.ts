5d3a71ca53c8e016a3d20edaa12c2c73b9a0c304
SupeemAFK <Peemlomsak@gmail.com>
2024-06-02 01:27:48 +0700

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import packageJson from './package.json'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES_PATH || (process.env.NODE_ENV === 'production'
-    ? '/openpose_editor/'
+    ? '/'
     : '/'),
   plugins: [
     vue(),
